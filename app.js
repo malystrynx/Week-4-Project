fab5db4cb949c934129d06e4c1c097c5ca65a7cb
Matthew Kusnadi <matthew.kusnadi@hotmail.com>
2023-04-29 06:12:31 +1000

    edit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // FAKE API
 https://www.omdbapi.com/?i=tt3896198&apikey=6d5a5da9
+https://www.omdbapi.com/?i=tt3896198&apikey=6d5a5da9&s=fast
 
+
+// NAVIGATION BAR SCROLL
 function changeScrolling(){
     var nav = document.getElementById('nav');
     var scrollValue = window.scrollY;
@@ -12,3 +15,16 @@ nav.classList.remove('scrolling');
 }
 
 window.addEventListener('scroll', changeScrolling);
+
+// MOVIES
+// References
+
+// GET RANDOM IMAGES IN FRONT SCREEN
+async function main(){
+    const users = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=6d5a5da9&s=fast");
+    const usersData = await users.json();
+    console.log(usersData)
+}
+
+main()
+
