7d837b3dceee36992e180f964ed79c92e6fae42c
Matthew Kusnadi <matthew.kusnadi@hotmail.com>
2023-05-23 19:45:41 +1000

    edit

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// FAKE API
-
-// https://www.omdbapi.com/?i=tt3896198&apikey=6d5a5da9
-
-// NAVIGATION BAR SCROLL
 function changeScrolling() {
   var nav = document.getElementById("nav");
   var scrollValue = window.scrollY;
@@ -20,17 +15,17 @@ async function onSearchChange(event) {
   const search = event.target.value;
   const movies = await fetch(
     `https://www.omdbapi.com/?i=tt3896198&apikey=6d5a5da9&s=${search}`
-    );
-    
-    const cardEl = document.querySelector(".cards");
-    cardEl.classList += ' movie__loading'
-    const moviesData = await movies.json(); 
-    const searchResults = moviesData.Search;
-    cardEl.classList.remove('movie__loading')
-    
-    
-    cardEl.innerHTML = searchResults
-    .slice(0, 6) 
+  );
+
+  const cardEl = document.querySelector(".cards");
+  const movieSpinner = document.querySelector(".movie__loading--wrapper");
+  cardEl.classList += " movie__loading";
+  const moviesData = await movies.json();
+  const searchResults = moviesData.Search;
+  cardEl.classList.remove("movie__loading");
+
+  cardEl.innerHTML = searchResults
+    .slice(0, 6)
     .map(
       (movie) => `
       <div class="card">
@@ -38,38 +33,35 @@ async function onSearchChange(event) {
       <div class="body data-body movieTitle"><h1>${movie.Title}</h1></div>
       <div class="body data-body"><h1>${movie.Year}</h1></div>
       </div>`
-      )
-      .join(""); 
-      const cardsContainer = document.querySelector(".cards");
-      cardsContainer.scrollIntoView({ behavior: "smooth" });
-    }
-  function searchMovies(event){
-    event.preventDefault();
-  }
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  async function main() {
-    const movies = await fetch(
-      "https://www.omdbapi.com/?i=tt3896198&apikey=6d5a5da9&s=fast"
-      );
-      const moviesData = await movies.json();
-      const cardEl = document.querySelector(".cards");
-      
-              cardEl.innerHTML = moviesData.Search
-              .slice(0, 6) 
-              .map(
-                (movie) => `
+    )
+    .join("");
+  const cardsContainer = document.querySelector(".cards");
+  cardsContainer.scrollIntoView({ behavior: "smooth" });
+}
+function searchMovies(event) {
+  event.preventDefault();
+}
+
+async function main() {
+  const loadingSpinner = document.querySelector(".movie__loading--wrapper");
+  const movies = await fetch(
+    "https://www.omdbapi.com/?i=tt3896198&apikey=6d5a5da9&s=fast"
+  );
+  const moviesData = await movies.json();
+  const cardEl = document.querySelector(".cards");
+  loadingSpinner.classList.remove("movie__loading");
+
+  cardEl.innerHTML = moviesData.Search.slice(0, 6)
+    .map(
+      (movie) => `
                 <div class="card">
                 <div class="header data-header"><img class="movie__search" src="${movie.Poster}" alt="This image is unavailable"></div>
                 <div class="body data-body movieTitle"><h1>${movie.Title}</h1></div>
                 <div class="body data-body"><h1>${movie.Year}</h1></div>
                 </div>
                 `
-                )
-                .join("");
-              }
-              
-              main();
-              
-              
-              // MAKE A SKELETON LOADING STAGE 
-              
+    )
+    .join("");
+}
+
+main();
